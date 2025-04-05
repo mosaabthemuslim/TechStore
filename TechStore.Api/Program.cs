acf5e1c15799a46f25373d321a965dfa4fb2c0ea
Mosaab <elbouamranimosaab@gmail.com>
2025-04-05 18:20:41 +0000

    Added User endpoints to swagger

--- TechStore.Api/Program.cs
+++ TechStore.Api/Program.cs
@@ -1,4 +1,6 @@
+using TechStore.Domain.User;
 using TechStore.Infrastructure.CollectionExtension;
+using TechStore.Infrastructure.DataBase;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddIdentityApiEndpoints<User>()
+    .AddEntityFrameworkStores<TechStoreDBContext>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -23,6 +28,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGroup("api/identity")
+        .WithTags("Identity")
+        .MapIdentityApi<User>();
 
 app.UseAuthentication();
 
--- TechStore.Infrastructure/CollectionExtension/ServiceCollectionExtention.cs
+++ TechStore.Infrastructure/CollectionExtension/ServiceCollectionExtention.cs
@@ -16,6 +16,7 @@ public static class ServiceCollectionExtention
         services.AddDbContext<TechStoreDBContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("TechStore")));
 
+
         return services;
     }
 }
